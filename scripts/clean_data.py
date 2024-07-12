3e0f1aadd619005e4dcc2785789a9e067f7b331c
Emir Mehmedćehajić <emir.mehmedcehajic@edu.fit.ba>
2024-07-13 01:38:02 +0200

    removed inplace arg due to warning being caused by it

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -6,10 +6,10 @@ def clean_data(df):
     """
     # Popuni nedostajuće vrijednosti za numeričke kolone sa medianom
     for column in df.select_dtypes(include=['float64', 'int64']).columns:
-        df[column].fillna(df[column].median(), inplace=True)
+        df[column] = df[column].fillna(df[column].median())
 
     # Popuni nedostajuće vrijednosti za kategorijske kolone sa najčešćim vrijednostima (mode)
     for column in df.select_dtypes(include=['object']).columns:
-        df[column].fillna(df[column].mode()[0], inplace=True)
+        df[column] = df[column].fillna(df[column].mode()[0])
 
     return df
